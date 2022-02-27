d60afb32b14b3e62682f9c092a83a2a34477b72e
21ciprian <2021ciprianoffice@gmail.com>
2022-02-27 23:09:53 +0000

    Created Home component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {ParallaxProvider} from 'react-scroll-parallax'
 import Header from '../Header'
+import Home from '../Home'
 import './App.css'
 
 function App() {
 	return (
-		<div className='app'>
-			<Header />
-		</div>
+		<ParallaxProvider>
+			<div className='app'>
+				<Header />
+				<Home />
+			</div>
+		</ParallaxProvider>
 	)
 }
 
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Home.module.css'
+
+type Props = {}
+
+function Home({}: Props) {
+	return (
+		<div className={style.home}>
+			<h2>Home</h2>
+		</div>
+	)
+}
+
+export default Home
