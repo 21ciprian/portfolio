5e9cb0cfbfe60352315857b872c8554065e1ce43
21ciprian <2021ciprianoffice@gmail.com>
2022-03-01 00:06:22 +0000

    imported sample video

---
+++ src/data/projectsData.ts
@@ -0,0 +1,37 @@
+import calc from '../assets/calc.png'
+import hangman from '../assets/hangman.png'
+import morse from '../assets/morse.jpg'
+// import r from '../assets/r1.mp4'
+
+const projectsData = [
+	{
+		id: 1,
+		name: 'Calculator',
+		imageSrc: calc,
+		description: 'Calculator made with Vanilla Javascript, HTML and SCSS',
+		gitLink: 'lorem',
+		liveSite: 'lorem',
+		video: '../assets/r1.mp4',
+	},
+	{
+		id: 2,
+		name: 'Morse Code Translator',
+		imageSrc: morse,
+		description:
+			'Morse Code Translator app made with Vanilla Javascript, HTML and SCSS. It allows you to convert english to morse code and morse code to english',
+		gitLink: 'lorem',
+		liveSite: 'lorem',
+		video: '../assets/r1.mp4',
+	},
+	{
+		id: 3,
+		name: 'Hangman Game',
+		imageSrc: hangman,
+		description: 'Hangman Game made with HTML, SCSS and Vanilla Javascript',
+		gitLink: 'lorem',
+		liveSite: 'lorem',
+		video: '../assets/r1.mp4',
+	},
+]
+
+export default projectsData
