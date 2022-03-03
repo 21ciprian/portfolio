bd87439c167f69f42070e271386f2bc8a800ba26
21ciprian <2021ciprianoffice@gmail.com>
2022-03-03 23:09:29 +0000

    Added display flex to About section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,34 +4,40 @@ import Title from '../Title'
 import styles from './About.module.css'
 function About() {
 	return (
-		<article id='about'>
-			<Title title='ABOUT ME' />
-			<section className={styles.about}>
-				<p>
-					Hi, I'm Ciprian, a <strong>full-stack developer</strong> and{' '}
-					<em>"a man of many trades"</em> as my friends would call me.
-				</p>
-				<p>
-					I am passionate about coding and problem solving and began teaching
-					myself how to code while I was working full time in the hospitality
-					industry.
-				</p>
-				<p>
-					I have recently graduated from{' '}
-					<a
-						href='https://www.schoolofcode.co.uk/'
-						target='_blank'
-						rel='noreferrer'>
-						<strong>School of Code</strong>
-					</a>{' '}
-					Software Development Bootcamp and I am excited to embark on a new
-					career as software developer.
-				</p>
+		<article id='about' className={styles.about__wrapper}>
+			<section className={styles.about__container}>
+				<section className={styles.about__text}>
+					<Title title='ABOUT ME' />
+					<p>
+						Hi, I'm Ciprian, a <strong>full-stack developer</strong> and{' '}
+						<em>"a man of many trades"</em> as my friends would call me.
+					</p>
+					<p>
+						I am passionate about coding and problem solving and began teaching
+						myself how to code while I was working full time in the hospitality
+						industry.
+					</p>
+					<p>
+						I have recently graduated from{' '}
+						<a
+							href='https://www.schoolofcode.co.uk/'
+							target='_blank'
+							rel='noreferrer'>
+							<strong>School of Code</strong>
+						</a>{' '}
+						Software Development Bootcamp and I am excited to embark on a new
+						career as software developer.
+					</p>
+				</section>
+				<div className={styles.image__container}>
+					{' '}
+					<img
+						src={programmer}
+						className={styles.programmer}
+						alt='programmer'
+					/>
+				</div>
 			</section>
-			<div className={styles.image__container}>
-				{' '}
-				<img src={programmer} className={styles.programmer} alt='programmer' />
-			</div>
 		</article>
 	)
 }
