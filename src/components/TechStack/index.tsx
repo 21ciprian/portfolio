c4ee174a7b1f5ff38ed0751cdc3e5bddbf723ca4
21ciprian <2021ciprianoffice@gmail.com>
2022-03-03 23:27:28 +0000

    Added svg to TechStack component

--- src/components/TechStack/index.tsx
+++ src/components/TechStack/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import software from '../../assets/software.svg'
 import Title from '../Title'
 import styles from './TechStack.module.css'
 
 function TechStack() {
 	return (
-		<section className={styles.tech}>
+		<article className={styles.tech}>
+			<div className={styles.image__container}>
+				{' '}
+				<img src={software} className={styles.software} alt='software' />
+			</div>
 			<Title title='TECH-STACK' />
-		</section>
+		</article>
 	)
 }
 
