83cf28fb75db40f4503dfb25f477b6baa141cccc
21ciprian <2021ciprianoffice@gmail.com>
2022-03-03 23:32:10 +0000

    Added default style to tech__wrapper

--- src/components/TechStack/index.tsx
+++ src/components/TechStack/index.tsx
@@ -5,12 +5,38 @@ import styles from './TechStack.module.css'
 
 function TechStack() {
 	return (
-		<article className={styles.tech}>
-			<div className={styles.image__container}>
-				{' '}
-				<img src={software} className={styles.software} alt='software' />
-			</div>
-			<Title title='TECH-STACK' />
+		<article className={styles.tech__wrapper}>
+			<section className={styles.tech__container}>
+				<div className={styles.image__container}>
+					{' '}
+					<img src={software} className={styles.software} alt='software' />
+				</div>
+				<section className={styles.tech__text}>
+					<Title title='TECH-STACK' />
+					<h5>Languages:</h5>
+					<p>Javascript ES6+, CSS3, HTML5</p>
+					<br />
+					<h5>Front-End:</h5>
+					<p>
+						React, Typescript, Styled-Components, Material UI, AOS, Sass(SCSS),
+						Bootstrap
+					</p>
+
+					<br />
+					<h5>Back-end:</h5>
+					<p>
+						Node.js, Express, REST API, Google Firebase, Firestore, MongoDB,
+						Hosting
+					</p>
+					<br />
+					<h5>Software Development:</h5>
+					<p>
+						Git & GitHub, Jira, Trello, NPM, Yarn, Babel, SPA, TDD, Jest,
+						Enzyme, OOP, Agile, Scrum, Pair programming, VS Code, Postman,
+						Figma, Canva.
+					</p>
+				</section>
+			</section>
 		</article>
 	)
 }
