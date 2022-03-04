d0ced135871a18efef5abf6a9372f95264a83179
21ciprian <2021ciprianoffice@gmail.com>
2022-03-04 00:09:14 +0000

    Created Contact component

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../Title'
+import styles from './Contact.module.css'
+
+function Contact() {
+	return (
+		<div className={styles.contact__container}>
+			<Title title='CONTACT' />
+		</div>
+	)
+}
+
+export default Contact
