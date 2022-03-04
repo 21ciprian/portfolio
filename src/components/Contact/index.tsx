0311ab9d8d2c3b75b6e118f7c629139f0a7e2142
21ciprian <2021ciprianoffice@gmail.com>
2022-03-04 00:20:23 +0000

    Added content to Contact component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import contact from '../../assets/contact.svg'
+import email from '../../assets/email.svg'
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/in.svg'
 import Title from '../Title'
 import styles from './Contact.module.css'
 
@@ -10,6 +14,27 @@ function Contact() {
 				PLEASE FEEL FREE TO CONTACT ME VIA THE LINKS BELOW FOR ANY PROJECT
 				COLLABORATION
 			</h5>
+			<div className={styles.contactMe}>
+				<img src={contact} alt='contact' className={styles.contact} />
+				<a
+					href='https://github.com/Ciprian-A'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<img src={github} alt='github' className={styles.github} />
+				</a>
+				<a
+					href='mailto:ciprianoffice10@gmail.com'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<img src={email} alt='email' className={styles.email} />
+				</a>
+				<a
+					href='https://www.linkedin.com/in/ciprian-andrei-8381231b9'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<img src={linkedin} alt='linkedin' className={styles.linkedin} />
+				</a>
+			</div>
 		</section>
 	)
 }
