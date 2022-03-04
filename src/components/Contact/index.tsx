ac9d9559f59370915c22c61c349e9560e1452058
21ciprian <2021ciprianoffice@gmail.com>
2022-03-04 00:23:24 +0000

    Styled h5 in contact__container

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -11,7 +11,7 @@ function Contact() {
 		<section className={styles.contact__container}>
 			<Title title='CONTACT' />
 			<h5>
-				PLEASE FEEL FREE TO CONTACT ME VIA THE LINKS BELOW FOR ANY PROJECT
+				FEEL FREE TO CONTACT ME VIA THE LINKS BELOW FOR ANY PROJECT
 				COLLABORATION
 			</h5>
 			<div className={styles.contactMe}>
