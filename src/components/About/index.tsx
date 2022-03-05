832941a0b36127ad615010f08bf1275acce5ac72
21ciprian <2021ciprianoffice@gmail.com>
2022-03-05 22:03:17 +0000

    Added parallax on tech stack

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,35 +1,40 @@
 import React from 'react'
+import {Parallax} from 'react-scroll-parallax'
 import programmer from '../../assets/dev.svg'
 import TechStack from '../TechStack'
 import Title from '../Title'
 import styles from './About.module.css'
+
 function About() {
 	return (
 		<article id='about' className={styles.about__wrapper}>
 			<section className={styles.about__container}>
-				<section className={styles.about__text}>
-					<Title title='ABOUT ME' />
-					<p>
-						Hi, I'm Ciprian, a <strong>full-stack developer</strong> and{' '}
-						<em>"a man of many trades"</em> as my friends would call me.
-					</p>
-					<p>
-						I am passionate about coding and problem solving and began teaching
-						myself how to code while I was working full time in the hospitality
-						industry.
-					</p>
-					<p>
-						I have recently graduated from{' '}
-						<a
-							href='https://www.schoolofcode.co.uk/'
-							target='_blank'
-							rel='noreferrer'>
-							<strong>School of Code</strong>
-						</a>{' '}
-						Software Development Bootcamp and I am excited to embark on a new
-						career as software developer.
-					</p>
-				</section>
+				<Parallax translateY={[-10, 55]}>
+					<section className={styles.about__text}>
+						<Title title='ABOUT ME' />
+						<p>
+							Hi, I'm Ciprian, a <strong>full-stack developer</strong> and{' '}
+							<em>"a man of many trades"</em> as my friends would call me.
+						</p>
+						<p>
+							I am passionate about coding and problem solving and began
+							teaching myself how to code while I was working full time in the
+							hospitality industry.
+						</p>
+						<p>
+							I have recently graduated from{' '}
+							<a
+								href='https://www.schoolofcode.co.uk/'
+								target='_blank'
+								rel='noreferrer'>
+								<strong>School of Code</strong>
+							</a>{' '}
+							Software Development Bootcamp and I am excited to embark on a new
+							career as software developer.
+						</p>
+					</section>
+				</Parallax>
+
 				<div className={styles.image__container}>
 					{' '}
 					<img
--- src/components/TechStack/index.tsx
+++ src/components/TechStack/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import software from '../../assets/soft.svg'
+import {Parallax} from 'react-scroll-parallax'
+import software from '../../assets/tech.svg'
 import Title from '../Title'
 import styles from './TechStack.module.css'
 
@@ -11,31 +12,33 @@ function TechStack() {
 					{' '}
 					<img src={software} className={styles.software} alt='software' />
 				</div>
-				<section className={styles.tech__text}>
-					<Title title='TECH-STACK' />
-					<h5>Languages:</h5>
-					<p>Javascript ES6+, CSS3, HTML5</p>
-					<br />
-					<h5>Front-End:</h5>
-					<p>
-						React, Typescript, Chakra, Styled-Components, MUI, Bootstrap,
-						Sass(SCSS), Netlify
-					</p>
+				<Parallax translateY={[-20, 10]}>
+					<section className={styles.tech__text}>
+						<Title title='TECH-STACK' />
+						<h5>Languages:</h5>
+						<p>Javascript ES6+, CSS3, HTML5</p>
+						<br />
+						<h5>Front-End:</h5>
+						<p>
+							React, Typescript, Chakra, Styled-Components, MUI, Bootstrap,
+							Sass(SCSS), Netlify
+						</p>
 
-					<br />
-					<h5>Back-end:</h5>
-					<p>
-						Node.js, Express.js, REST API, PostgreSQL, OAuth, Heroku, Postman.
-					</p>
-					<br />
-					<h5>Testing Libraries:</h5>
-					<p>Jest, SuperTest, React Testing Library, Cypress.</p>
-					<h5>Software Development:</h5>
-					<p>
-						Git & GitHub, Jira, Trello, NPM, Yarn, SPA, TDD, Agile, Scrum, Pair
-						programming, VS Code, Figma, Canva, Draw.io.
-					</p>
-				</section>
+						<br />
+						<h5>Back-end:</h5>
+						<p>
+							Node.js, Express.js, REST API, PostgreSQL, OAuth, Heroku, Postman.
+						</p>
+						<br />
+						<h5>Testing Libraries:</h5>
+						<p>Jest, SuperTest, React Testing Library, Cypress.</p>
+						<h5>Software Development:</h5>
+						<p>
+							Git & GitHub, Jira, Trello, NPM, Yarn, SPA, TDD, Agile, Scrum,
+							Pair programming, VS Code, Figma, Canva, Draw.io.
+						</p>
+					</section>
+				</Parallax>
 			</section>
 		</article>
 	)
